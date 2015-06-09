46d66292b309cc58b9b9e4a8211236effc77fcaa
Kevin Rousseau <rousseau.kevin15@gmail.com>
2015-06-09 11:56:52 +0200

    header scrollable + wheel & clouds animated - closes #1, #7

--- _js/app.js
+++ _js/app.js
@@ -1,8 +1,55 @@
 (function(){
 
+	var svgArea = Snap('.intro-svg');
+
+
 	function init(){
-		console.log('[app.js]');
+
+		rotateRad();
+
+		moveClouds();
+
 	}
+
+	function moveClouds(){
+
+		var clouds = svgArea.select('#wolken_20'),
+		cloudBbox = clouds.getBBox();
+
+		clouds.transform('t200, 0');
+
+		clouds.animate(
+			{ transform: 't5000,0' },
+			200000,
+			function(){
+				clouds.attr(
+					{ transform: 'position(0 ' + cloudBbox.x + ' ' + cloudBbox.y + ')' }
+				);
+
+				moveClouds();
+			});
+	}
+
+
+	function rotateRad(){
+
+		var rad = svgArea.select('#rad'),
+		radBbox = rad.getBBox();
+
+		rad.animate(
+			{ transform: "r360," + radBbox.cx + ',' + radBbox.cy },
+			20000,
+			function(){
+				rad.attr(
+					{ transform: 'rotate(0 ' + radBbox.cx + ' ' + radBbox.cy + ')' }
+				);
+
+				rotateRad();
+			}
+		);
+
+	}
+
 
 	init();
 
--- js/init.js
+++ js/init.js
@@ -1,11 +1,18 @@
 fallback.load({
 	jQuery: [
 		'http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js',
-		'vendor/jquery/dist/jquery.min.js'
+		'js/vendor/jquery/dist/jquery.min.js'
+	],
+	Snap: [
+		'http://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.3.0/snap.svg-min.js',
+		'js/vendor/snap.svg/dist/snap.svg-min.js'
+	],
+	Lazysizes: [
+		'js/vendor/lazysizes/lazysizes.min.js'
 	],
 	'app.js': 'js/app.js'
 },{
 	shim: {
-		'app.js': ['jQuery']
+		'app.js': ['jQuery', 'Snap']
 	}
 })
